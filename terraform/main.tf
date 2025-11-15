13a57e589f1f6301bc575fba434bf0e1539b777a
manuraj23 <manuraj082004@gmail.com>
2025-11-16 00:06:28 +0530

    Test 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,42 +1,3 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = var.public_key
-}
-
-resource "aws_security_group" "allow_http_ssh" {
-  name        = "brainwave-sg"
-  description = "Allow SSH and HTTP"
-  ingress {
-    description = "ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "http"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 resource "aws_instance" "app_server" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
@@ -47,5 +8,3 @@ resource "aws_instance" "app_server" {
     Name = "brainwave-app-server"
   }
 }
-
-
