087355c1d7d3afa647894b452fd028d2251fb648
manuraj23 <manuraj082004@gmail.com>
2025-11-17 23:36:47 +0530

    Test 17

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,14 +20,12 @@ resource "aws_security_group" "allow_http_ssh" {
   name        = "brainwave-sg-${random_id.suffix.hex}"
   description = "Allow SSH and HTTP"
   ingress {
-    description = "ssh"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    description = "http"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -39,6 +37,7 @@ resource "aws_security_group" "allow_http_ssh" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
 }
 
 
